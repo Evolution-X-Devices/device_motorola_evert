bc3c525621cc74f5ca9fd5c96fbc2e6df5c0cec9
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    evert: Migrate to AIDL LiveDisplay SDM/sysfs HALs
    Change-Id: I580e0721b050480ed5a9ca8bb145f9ef1f1b174f

--- device.mk
+++ device.mk
@@ -56,6 +56,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.msm8998
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
+$(call soong_config_set,livedisplay_sysfs,enable_se,true)
+
 # Moto Camera 2
 TARGET_MOTCAMERA2 := primary
 TARGET_USES_MOTCAMERA2 := true
